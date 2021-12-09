0dfcfe1f35991d4c9ac4c2385681d65984d9bb40
albertaljeanpierre <albertaljeanpierre@gmail.com>
2021-12-09 20:38:44 +0100

    Creation des tables catégorie et prestataire

---
+++ src/Entity/Categorie.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategorieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategorieRepository::class)
+ */
+class Categorie
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $enAvant;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $validite;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Prestataire::class, inversedBy="categories")
+     */
+    private $prestataire;
+
+    public function __construct()
+    {
+        $this->prestataire = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getEnAvant(): ?bool
+    {
+        return $this->enAvant;
+    }
+
+    public function setEnAvant(bool $enAvant): self
+    {
+        $this->enAvant = $enAvant;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getValidite(): ?bool
+    {
+        return $this->validite;
+    }
+
+    public function setValidite(bool $validite): self
+    {
+        $this->validite = $validite;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Prestataire[]
+     */
+    public function getPrestataire(): Collection
+    {
+        return $this->prestataire;
+    }
+
+    public function addPrestataire(Prestataire $prestataire): self
+    {
+        if (!$this->prestataire->contains($prestataire)) {
+            $this->prestataire[] = $prestataire;
+        }
+
+        return $this;
+    }
+
+    public function removePrestataire(Prestataire $prestataire): self
+    {
+        $this->prestataire->removeElement($prestataire);
+
+        return $this;
+    }
+}
---
+++ src/Entity/Prestataire.php
@@ -0,0 +1,131 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PrestataireRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PrestataireRepository::class)
+ */
+class Prestataire
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $siteInternet;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $numTel;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $numTVA;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Categorie::class, mappedBy="prestataire")
+     */
+    private $categories;
+
+    public function __construct()
+    {
+        $this->categories = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getSiteInternet(): ?string
+    {
+        return $this->siteInternet;
+    }
+
+    public function setSiteInternet(?string $siteInternet): self
+    {
+        $this->siteInternet = $siteInternet;
+
+        return $this;
+    }
+
+    public function getNumTel(): ?string
+    {
+        return $this->numTel;
+    }
+
+    public function setNumTel(?string $numTel): self
+    {
+        $this->numTel = $numTel;
+
+        return $this;
+    }
+
+    public function getNumTVA(): ?string
+    {
+        return $this->numTVA;
+    }
+
+    public function setNumTVA(?string $numTVA): self
+    {
+        $this->numTVA = $numTVA;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Categorie[]
+     */
+    public function getCategories(): Collection
+    {
+        return $this->categories;
+    }
+
+    public function addCategory(Categorie $category): self
+    {
+        if (!$this->categories->contains($category)) {
+            $this->categories[] = $category;
+            $category->addPrestataire($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCategory(Categorie $category): self
+    {
+        if ($this->categories->removeElement($category)) {
+            $category->removePrestataire($this);
+        }
+
+        return $this;
+    }
+}
