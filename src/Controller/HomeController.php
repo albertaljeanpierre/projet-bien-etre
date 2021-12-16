85164a3e95f36f4bbf013946586b440b007d8d51
albertaljeanpierre <albertaljeanpierre@gmail.com>
2021-12-16 20:07:42 +0100

    Affichage des catégories

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Categorie;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $repository = $entityManager->getRepository(Categorie::class);
+        $categories = $repository->findAll();
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'categories' => $categories,
         ]);
     }
 }
